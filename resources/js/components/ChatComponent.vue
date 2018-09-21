3df00e5f914d7f1b38e6054bd0e8d85352f0b3d3
VDovhan23 <vdovhan23@gmail.com>
2018-09-21 11:24:57 +0300

    block claer

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-3">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Private chat
+                    </div>
+                    <ul class="list-group">
+                        <!--<a href=""-->
+                           <!--@click.prevent="openChat(user)"-->
+                           <!--:key="user.id"-->
+                           <!--v-for="user in users">-->
+                            <li class="list-group-item"> GENHJ</li>
+                        <!--</a>-->
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <!--<span v-for="user in users" :key="user.id" v-if="user.session">-->
+                <message-component
+                        v-if="open"
+                        @close="close"
+                ></message-component>
+            <!--</span>-->
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import MessageComponent from "./MessageComponent";
+    export default {
+        data(){
+            return{
+                open: true
+            }
+        },
+        methods:{
+            close(){
+                this.open = false
+            }
+        },
+        created(){
+
+        },
+        components: { MessageComponent }
+    }
+</script>
---
+++ resources/js/components/MessageComponent.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="card card-default chat-box" >
+        <div class="card-header">
+            <b :class="{'text-danger' :block_session}">
+                <!--{{user.name}}-->
+                <span v-if="block_session">(Blocked)</span>
+            </b>
+            <!--close button-->
+            <a href="" @click.prevent='close'>
+                <i class="fas fa-times float-right" ></i>
+            </a>
+
+            <div class="dropdown float-right mr-4" >
+                <a href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-ellipsis-v "></i>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#" v-if="block_session" @click.prevent='unblock' >UnBlock User</a>
+                    <a class="dropdown-item" href="#" v-else @click.prevent='block'>Block User</a>
+                    <a class="dropdown-item" href="#" @click.prevent='clear'>Clear Chat</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="card-body" v-chat-scroll>
+            <p class="card-text" v-for="chat in chats" :key="chat.message">
+                {{chat.message}}
+            </p>
+        </div>
+        <form  class="card-footer" @submit.prevent="send">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Write your message" :disabled="block_session">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                chats:[],
+                block_session: false
+            }
+        },
+        methods: {
+            send(){
+                console.log("yes")
+            },
+            close(){
+                this.$emit('close');
+            },
+            clear(){
+                this.chats = [];
+            },
+            block(){
+                this.block_session = true
+            },
+            unblock(){
+                this.block_session = false
+            }
+        },
+        created(){
+            this.chats.push(
+                {message: "heyy"},
+                {message: "heyaay"},
+                {message: "heyy you to"},
+                {message: "yyyyyyyyyy"},
+                {message: "heyasdady"},
+                {message: "heyasasdaddsdady"},
+                {message: "heyasaasdaasdasdady"},
+                {message: "heyasasasdasdaasaddy"},
+                {message: "heyasaasdasdsdasdady"},
+                {message: "heyasasaaadasdady"},
+            )
+        }
+    }
+</script>
+
+<style scoped>
+    .chat-box{
+        height: 400px;
+    }
+    .card-body{
+        overflow-y: scroll;
+    }
+</style>
